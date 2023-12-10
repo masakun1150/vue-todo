71b9d10e15816b5607022269d0bbff8671b306ff
User <user@yuzanoMacBook-Air.local>
2023-12-10 15:33:25 +0900

    必要なvueだけ残す

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Todo from '@/views/Todo.vue'
+import Todo from '@/components/Todo.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
