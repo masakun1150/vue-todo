5490ea5ce223ae71652dcf793c14ceee55dd1133
User <user@yuzanoMacBook-Air.local>
2023-12-10 15:44:20 +0900

    でも用のvue test コード作成

--- src/components/__tests__/Todo.vue.spec.ts
+++ src/components/__tests__/Todo.vue.spec.ts
@@ -1,28 +1,47 @@
 import { mount } from '@vue/test-utils';
 import Todo from '@/components/Todo.vue';
+import { describe, it, expect } from  'vitest';
 
-describe('Todo.vue', () => {
-  it('renders todo items correctly', async () => {
-    const wrapper = mount(Todo);
 
-    // Add a new task
-    await wrapper.setData({ newTodo: 'Test Task' });
-    await wrapper.vm.$nextTick();
-    wrapper.find('input').trigger('keyup.enter');
+// github copilotの補完をもとに生成したテストコード
+// 上で改行してgithub copilotが生成したテストコードブロックを展開してみる
 
-    // Check if the task is rendered
-    expect(wrapper.text()).toContain('Test Task');
-  });
+// describe('Todo.vue', () => {
+//   // given
+//   const wrapper = mount(Todo);
+//   const newTodo = 'Test Task';
 
-  it('does not render empty tasks', async () => {
-    const wrapper = mount(Todo);
+//   // when
+//   wrapper.setData({ newTodo });
+//   wrapper.find('input').trigger('keyup.enter');
 
-    // Add an empty task
-    await wrapper.setData({ newTodo: '' });
-    await wrapper.vm.$nextTick();
-    wrapper.find('input').trigger('keyup.enter');
+//   // then
+//   expect(wrapper.text()).toContain(newTodo);
+// });
 
-    // Check if the empty task is not rendered
-    expect(wrapper.text()).not.toContain('');
-  });
-});
+// chatGPTが生成したテストコード
+// describe('Todo.vue', () => {
+//   it('renders todo items correctly', async () => {
+//     const wrapper = mount(Todo);
+
+//     // Add a new task
+//     await wrapper.setData({ newTodo: 'Test Task' });
+//     await wrapper.vm.$nextTick();
+//     wrapper.find('input').trigger('keyup.enter');
+
+//     // Check if the task is rendered
+//     expect(wrapper.text()).toContain('Test Task');
+//   });
+
+//   it('does not render empty tasks', async () => {
+//     const wrapper = mount(Todo);
+
+//     // Add an empty task
+//     await wrapper.setData({ newTodo: '' });
+//     await wrapper.vm.$nextTick();
+//     wrapper.find('input').trigger('keyup.enter');
+
+//     // Check if the empty task is not rendered
+//     expect(wrapper.text()).not.toContain('');
+//   });
+// });
